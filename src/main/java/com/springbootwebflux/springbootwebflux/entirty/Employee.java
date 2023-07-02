54b05f3ed8a832c57559b28481b8be597cae1aac
mohosinmiah <hamza1610330816@gmail.com>
2023-07-02 18:52:38 +0600

    Employee Entity Added

---
+++ src/main/java/com/springbootwebflux/springbootwebflux/entirty/Employee.java
@@ -0,0 +1,21 @@
+package com.springbootwebflux.springbootwebflux.entirty;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collation = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    
+    private String Id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
