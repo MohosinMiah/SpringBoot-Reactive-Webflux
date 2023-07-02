5295785f8fbd5354e5b64f7c8c833cc99281926b
mohosinmiah <hamza1610330816@gmail.com>
2023-07-02 18:40:47 +0600

    Initial project setup

---
+++ src/main/java/com/springbootwebflux/springbootwebflux/SpringbootwebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.springbootwebflux.springbootwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootwebflux/springbootwebflux/SpringbootwebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootwebflux.springbootwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
